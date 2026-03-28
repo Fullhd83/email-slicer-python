8dfb1f1e3e36dacd9b139beffe422bc74c8031a2
Homi <homayoundaliri@gmail.com>
2026-03-28 07:45:17 +0000

    added a while loop to ask if a user want to enter another email at the end

--- email_slice.py
+++ email_slice.py
@@ -1,24 +1,31 @@
-email = input('enter your email here: ')
+while True:
 
-#just testing the email for validity, not checking for all the rules of email validation
+    email = input('enter your email here: ')
 
-if email.count('@') != 1:
+    if email.count('@') != 1:
     
-    print('invalid email')
+        print('invalid email')
 
-elif " " in email:
-    print('email should not have a space')
+    elif " " in email:
+        print('email should not have a space')
 
 
-else:
-    separated_email = email.split('@')
-    username = separated_email[0]
-    domain = separated_email[1]
-    
-    if username == '' or domain == '':
-        print('email should have a username and domain')
-    
     else:
+        separated_email = email.split('@')
+        username = separated_email[0]
+        domain = separated_email[1]
+    
+        if username == '' or domain == '':
+            print('email should have a username and domain')
+    
+        else:
 
-        print(f'username: {username}')
-        print(f'domain: {domain}')
+            print(f'username: {username}')
+            print(f'domain: {domain}')
+    
+    print('do you want to enter another email? (yes/no) (y/n)')
+    answer = input().lower()
+    
+    if answer != 'yes' and answer != 'y':
+        break
+        
