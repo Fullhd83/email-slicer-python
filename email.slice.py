68f45e9ab9e211736b1a86b93b0b28f769c2f801
Homi <homayoundaliri@gmail.com>
2026-03-28 06:28:37 +0000

    first email slicer project

---
+++ email.slice.py
@@ -0,0 +1,22 @@
+email = input('enter your email here: ')
+
+if email.count('@') != 1:
+    
+    print('invalid email')
+
+elif " " in email:
+    print('email should not have a space')
+
+
+else:
+    separated_email = email.split('@')
+    username = separated_email[0]
+    domain = separated_email[1]
+    
+    if username == '' or domain == '':
+        print('email should have a username and domain')
+    
+    else:
+
+        print(f'username: {username}')
+        print(f'domain: {domain}')
